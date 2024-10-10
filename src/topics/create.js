2a91a45dd5d8c1aeac489cbea733ef973f9b3a92
elliay <elyang1221@gmail.com>
2024-10-09 21:20:07 -0400

    Commenting out useless code for testing

--- src/topics/create.js
+++ src/topics/create.js
@@ -320,17 +320,17 @@ module.exports = function (Topics) {
 		}
 	}
 
-	Topics.updateVisibility = async function (tid, uid, isprivate) {
-		const topic = await Topics.getTopicFields(tid, ['tid', 'uid', 'private']);
-		// Check if the user is the owner of the topic or has admin privileges
-		const isOwner = topic.uid === uid;
-		const isAdmin = await privileges.users.isAdministrator(uid);
-		if (!isOwner && !isAdmin) {
-			throw new Error('[[error:no-privileges]]');
-		}
-		topic.private = isprivate;
-		await db.setObjectField(`topic:${tid}`, 'private', isprivate);
-		// await plugins.hooks.fire('action:topic.visibilityChange', { tid, uid, isprivate });
-		return topic;
-	};
+	// Topics.updateVisibility = async function (tid, uid, isprivate) {
+	// 	const topic = await Topics.getTopicFields(tid, ['tid', 'uid', 'private']);
+	// 	// Check if the user is the owner of the topic or has admin privileges
+	// 	const isOwner = topic.uid === uid;
+	// 	const isAdmin = await privileges.users.isAdministrator(uid);
+	// 	if (!isOwner && !isAdmin) {
+	// 		throw new Error('[[error:no-privileges]]');
+	// 	}
+	// 	topic.private = isprivate;
+	// 	await db.setObjectField(`topic:${tid}`, 'private', isprivate);
+	// 	// await plugins.hooks.fire('action:topic.visibilityChange', { tid, uid, isprivate });
+	// 	return topic;
+	// };
 };
