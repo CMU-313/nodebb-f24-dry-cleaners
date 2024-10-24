552271bb74d26f20e0d8c932f5a8f5163894eeed
Yiyao <ikearsu@gmail.com>
2024-10-23 20:48:49 -0400

    installed cucumber testing tool and created an initla draft test file

---
+++ features/step_definitions/create_topic_steps.js
@@ -0,0 +1,17 @@
+const { Given, Then } = require('@cucumber/cucumber');
+const assert = require('assert');
+const { Builder, By } = require('selenium-webdriver');
+
+let driver;
+
+Given('I am on the home page', async function () {
+  driver = await new Builder().forBrowser('chrome').build();
+  await driver.get('http://localhost:4567'); // Replace with your actual home page URL
+});
+
+Then('I should see {string}', async function (expectedText) {
+  const element = await driver.wait(until.elementLocated(By.tagName('body')), 10000);
+  const bodyText = await element.getText();
+  assert(bodyText.includes(expectedText), `Expected to see "${expectedText}", but got "${bodyText}"`);
+  await driver.quit();
+});
