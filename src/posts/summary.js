52968a1cd37b58e2bb577d56c07592dcb82ce174
elliay <elyang1221@gmail.com>
2024-11-14 16:29:30 -0500

    Added the new fields everywhere I could find :(((

--- src/posts/summary.js
+++ src/posts/summary.js
@@ -19,7 +19,7 @@ module.exports = function (Posts) {
 		options.parse = options.hasOwnProperty('parse') ? options.parse : true;
 		options.extraFields = options.hasOwnProperty('extraFields') ? options.extraFields : [];
 
-		const fields = ['pid', 'tid', 'content', 'uid', 'timestamp', 'deleted', 'upvotes', 'downvotes', 'replies', 'handle', 'endorsed'].concat(options.extraFields);
+		const fields = ['pid', 'tid', 'content', 'uid', 'timestamp', 'deleted', 'upvotes', 'downvotes', 'replies', 'handle', 'endorsed', 'isEnglish', 'translatedContent'].concat(options.extraFields);
 
 		let posts = await Posts.getPostsFields(pids, fields);
 		posts = posts.filter(Boolean);
