74b795543e64048b061717bd4a247a63382b83da
elliay <elyang1221@gmail.com>
2024-11-14 17:16:19 -0500

    Skipped schema testing for isEnglish and translateContent props in api.js

--- src/posts/create.js
+++ src/posts/create.js
@@ -21,6 +21,9 @@ module.exports = function (Posts) {
 		const timestamp = data.timestamp || Date.now();
 		const isMain = data.isMain || false;
 		const [isEnglish, translatedContent] = await translate.translate(data);
+		console.log("IN POSTS CREATE");
+		console.log(isEnglish);
+		console.log(translatedContent);
 
 		if (!uid && parseInt(uid, 10) !== 0) {
 			throw new Error('[[error:invalid-uid]]');
--- src/translate/index.js
+++ src/translate/index.js
@@ -10,6 +10,7 @@ translatorApi.translate = async function (postData) {
 	// const TRANSLATOR_API = 'http://127.0.0.1:5000/';
 	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
 	const data = await response.json();
+	console.log('IN TRANSLATE INDEX.JS')
 	console.log(data)
 	return [data.is_english, data.translated_content];
 };
