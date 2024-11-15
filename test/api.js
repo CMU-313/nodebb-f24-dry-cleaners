a1b263fac6cbaef8414192ea9fd25576a7e06447
Daniel Sung <dksung@andrew.cmu.edu>
2024-11-15 01:44:40 -0500

    removed unrelated meta test

--- test/api.js
+++ test/api.js
@@ -472,19 +472,6 @@ describe('API', async () => {
 					}
 				});
 
-				it('should successfully re-login if needed', async () => {
-					const reloginPaths = ['GET /api/user/{userslug}/edit/email', 'PUT /users/{uid}/password', 'DELETE /users/{uid}/sessions/{uuid}'];
-					if (reloginPaths.includes(`${method.toUpperCase()} ${path}`)) {
-						({ jar } = await helpers.loginUser('admin', '123456'));
-						const sessionIds = await db.getSortedSetRange('uid:1:sessions', 0, -1);
-						const sessObj = await db.sessionStoreGet(sessionIds[0]);
-						const { uuid } = sessObj.meta;
-						mocks.delete['/users/{uid}/sessions/{uuid}'][1].example = uuid;
-
-						// Retrieve CSRF token using cookie, to test Write API
-						csrfToken = await helpers.getCsrfToken(jar);
-					}
-				});
 			});
 		});
 	}
